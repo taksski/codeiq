0d2e9fa773610eadf3296416a6f13b28a50e4d4a
SASAKI, Takeshi <sasaki@tsaw.jp>
2013-09-02 20:46:28 +0900

    add crossing

---
+++ crossing/crossing.rb
@@ -0,0 +1,26 @@
+def count(a,n)
+  return 0 if a.size < 2
+  count = 0
+  low = []
+  high = []
+  a.each { |i|
+    if (i > n)
+      high.push i
+    else
+      low.push i
+      count += high.size
+    end
+  }
+  count + count(low, n-(low.size/2)) + count(high, n+(high.size/2))
+end
+
+cnt = 0
+begin_time = Time.now
+# puts begin_time
+open("crossing.txt", "r") do |file|
+  a = file.map { |line| line.chop.to_i }
+  cnt = count(a, a.size / 2)
+end
+end_time = Time.now
+# puts end_time
+puts "#{cnt},#{(end_time-begin_time).to_i}"
